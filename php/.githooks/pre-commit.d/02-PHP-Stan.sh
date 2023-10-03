55755dc4761e5ef6357481c411adba9f548feb3d
Raymond Olavides <106138031+rofsti@users.noreply.github.com>
2023-10-03 10:10:02 +0800

    Update 02-PHP-Stan.sh
    Adjusted error message.

--- php/.githooks/pre-commit.d/02-PHP-Stan.sh
+++ php/.githooks/pre-commit.d/02-PHP-Stan.sh
@@ -4,7 +4,7 @@ echo "Inspect code using PHPStan"
 # Run `./vendor/bin/phpstan analyse --memory-limit=2G` command
 ./vendor/bin/phpstan analyse --memory-limit=2G
 if [ $? -ne 0 ]; then
-  echo "Maven verify failed. Please fix the issues before committing."
+  echo "PHPStan analyze failed. Please fix the issues before committing."
   echo ""
   exit 1
 fi
